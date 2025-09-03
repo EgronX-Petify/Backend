9f84add535ab67f1a2bf126565d3fb3c506a78fd
abdallah <a.m9518520963@outlook.com>
2025-09-03 10:52:52 +0300

    init project

---
+++ src/test/java/com/example/Petify/PetifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Petify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
